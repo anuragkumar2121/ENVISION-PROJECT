cb08b829b0dd54e1bac2c3c6b6677d7e75db1092
anuragkumar2121 <anuragkumar.201it209@nitk.edu.in>
2021-09-28 21:59:22 +0530

    Minor Changes

--- bus/urls.py
+++ bus/urls.py
@@ -13,6 +13,4 @@ urlpatterns = [
     path("login", views.login_view, name = "login"),
     path("logout", views.logout_view, name="logout"),
     path("profile", views.profile, name="profile")
-
-
 ]
